bb1e8b27238a481ee0a4bc9c20b1daa5f6a2b5cd
aknair95 <aakashunair01031995@gmail.com>
2023-01-25 21:17:40 +0530

    first js add

---
+++ demo-dom.js
@@ -0,0 +1,6 @@
+let header=document.getElementById('main-header');
+header.style.borderBottom='solid black 2px';
+
+let title=document.querySelector('.title');
+title.style.fontWeight='bold';
+title.style.color='green';
