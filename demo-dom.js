60907f8a7f780b4f532247d3096d0bb20881010c
aknair95 <aakashunair01031995@gmail.com>
2023-01-27 01:07:58 +0530

    task-5 edit

--- demo-dom.js
+++ demo-dom.js
@@ -5,8 +5,8 @@ let title=document.querySelector('.title');
 title.style.fontWeight='bold';
 title.style.color='green';
 
-let element=document.querySelectorAll('.list-group-item');
-element[2].style.background='green';
+let element=document.querySelector('.list-group-item:nth-child(2)');
+element.style.backgroundColor='green';
 
 let elementall=document.querySelectorAll('.list-group-item');
 for(let i=0;i<elementall.length;i++)
@@ -14,3 +14,8 @@ for(let i=0;i<elementall.length;i++)
     elementall[i].style.fontWeight='bold';
     elementall[i].style.color='blue';
 }
+let li=document.getElementsByTagName('li');
+li[4].style.color='red';
+li[4].style.fontWeight='bold';
+li[4].style.backgroundColor='#ccc'
+
