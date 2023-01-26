a12469f1492a7f7db7ec449ef5e240fca3458461
aknair95 <aakashunair01031995@gmail.com>
2023-01-26 19:00:33 +0530

    dom-list-font-color-background change

--- demo-dom.js
+++ demo-dom.js
@@ -4,3 +4,13 @@ header.style.borderBottom='solid black 2px';
 let title=document.querySelector('.title');
 title.style.fontWeight='bold';
 title.style.color='green';
+
+let element=document.querySelectorAll('.list-group-item');
+element[2].style.background='green';
+
+let elementall=document.querySelectorAll('.list-group-item');
+for(let i=0;i<elementall.length;i++)
+{
+    elementall[i].style.fontWeight='bold';
+    elementall[i].style.color='blue';
+}
