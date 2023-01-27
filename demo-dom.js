008e1af19cf42eda0f0987afd72938e1015fcda2
aknair95 <aakashunair01031995@gmail.com>
2023-01-27 09:05:25 +0530

    task7

--- demo-dom.js
+++ demo-dom.js
@@ -1,3 +1,4 @@
+/*
 let header=document.getElementById('main-header');
 header.style.borderBottom='solid black 2px';
 
@@ -30,3 +31,74 @@ for(let i=0;i<odd.length;i++)
 {
     odd[i].style.backgroundColor='green';
 }
+*/
+
+//parentnode
+    let parent=document.getElementById('items');
+    console.log(parent.parentNode);
+    parent.parentNode.style.backgroundColor='#ccc';
+
+//parentelement
+    let parent=document.getElementById('items');
+    console.log(parent.parentElement);
+    parent.parentElement.style.backgroundColor='#ccc';
+//childnodes
+    let child=document.getElementById('items');
+    console.log(child.childNodes);
+    child.childNodes[5].style.backgroundColor='yellow';
+//children
+    let child1=document.getElementById('items');
+    console.log(child1.children);
+    child1.children[3].style.backgroundColor="yellow";
+ //firstchild
+    console.log(child1.firstChild);
+ //firstelementchild
+    console.log(child1.firstElementChild);
+    child1.firstElementChild.style.fontSize='20px';
+ //lastchild
+    console.log(child1.lastChild); 
+ //lastelementchild
+    console.log(child1.lastElementChild);
+    child1.lastElementChild.style.fontSize='25px';
+ //nextsibling
+    console.log(child1.nextSibling);
+ //nextelementsibling
+    let nextsib=document.querySelector('.list-group-item');   
+    nextsib.nextElementSibling.style.fontWeight='bold';
+ //previoussibling
+    let prevsib=document.querySelector('.form-inline');
+ //previouselementsibling   
+    prevsib.previousElementSibling.textContent='ADD ITEMS -';
+  
+ //creatingelement
+    let newli=document.createElement('div');
+    newli.id='newli';
+    newli.className='list';
+    newli.setAttribute('title','newlist');
+ //createtextnode   
+    let textnode=document.createTextNode('Hello');
+ //connecting element & textnode   
+    newli.appendChild(textnode);
+    console.log(newli); 
+    
+ //adding hello before item lister
+    let pos1=document.querySelector('.container');
+    let pos2=document.querySelector('h1'); 
+    pos1.insertBefore(newli,pos2);
+    newli.style.color='black';
+    newli.style.backgroundColor='lightblue'
+  
+ //adding hello before item 1
+
+    let newli1=document.createElement('div');
+    newli1.id='newli1';
+    newli1.className='list-group-item';
+    newli1.setAttribute('title','newlist');
+    let textnode1=document.createTextNode('Hello');  
+    newli1.appendChild(textnode1);
+    console.log(newli1);
+
+    let pos3=document.querySelector('.list-group');
+    let pos4=document.querySelector('.list-group-item');
+    pos3.insertBefore(newli1,pos4);
+      
