25097d9f06e00a8b78cedffb22567527fb1bf174
aknair95 <aakashunair01031995@gmail.com>
2023-01-30 15:34:33 +0530

    task-14

--- formUserInfo.js
+++ formUserInfo.js
@@ -6,6 +6,7 @@ let userlist=document.getElementById('userInfoList');
 
 userform.addEventListener('submit',storeList);
 userlist.addEventListener('click',remove);
+userlist.addEventListener('click',edit);
 
 function storeList(e)
 {   
@@ -23,12 +24,18 @@ function storeList(e)
     liTag.id=userMobileNo.value;
     let liText=document.createTextNode(`Name-${userName.value} EmailID-${userEmail.value} Mobile No-${userMobileNo.value}`);
 
+    let edit=document.createElement('input');
+    edit.type='button';
+    edit.className='edit-btn';
+    edit.value='Edit';
+
     let del=document.createElement('input');
     del.type='button';
     del.className='del-btn';
     del.value='Delete';
 
     liTag.appendChild(liText);
+    liTag.appendChild(edit);
     liTag.appendChild(del);
     userlist.appendChild(liTag);
     
@@ -45,4 +52,20 @@ function remove(e1)
         userlist.removeChild(delLi);
         localStorage.removeItem(delLi.id);
     }  
+}
+
+function edit(e2)
+{
+    if(e2.target.classList.contains('edit-btn'))
+    {
+        let editLi=e2.target.parentElement;
+        userlist.removeChild(editLi);
+        let LSData=localStorage.getItem(editLi.id);
+        let LSDataObj=JSON.parse(LSData);
+        localStorage.removeItem(editLi.id);
+        userName.value=LSDataObj.Name;
+        userEmail.value=LSDataObj.Email;
+        userMobileNo.value=LSDataObj.MobileNo;
+    }
+
 }
