125862e801e728bf08ad3f5ef035ae0a59afa396
aknair95 <aakashunair01031995@gmail.com>
2023-01-30 09:04:19 +0530

    task-13

--- formUserInfo.js
+++ formUserInfo.js
@@ -5,6 +5,7 @@ let userform=document.getElementById('addform');
 let userlist=document.getElementById('userInfoList');
 
 userform.addEventListener('submit',storeList);
+userlist.addEventListener('click',remove);
 
 function storeList(e)
 {   
@@ -16,12 +17,32 @@ function storeList(e)
     }
     let UserDataString=JSON.stringify(userData);
     localStorage.setItem(userMobileNo.value,UserDataString);
+
     let liTag=document.createElement('li');
     liTag.className='list-group-item';
+    liTag.id=userMobileNo.value;
     let liText=document.createTextNode(`Name-${userName.value} EmailID-${userEmail.value} Mobile No-${userMobileNo.value}`);
+
+    let del=document.createElement('input');
+    del.type='button';
+    del.className='del-btn';
+    del.value='Delete';
+
     liTag.appendChild(liText);
+    liTag.appendChild(del);
     userlist.appendChild(liTag);
+    
     userName.value='';
     userEmail.value='';
     userMobileNo.value='';
+}
+
+function remove(e1)
+{   
+    if(e1.target.classList.contains('del-btn'))
+    {
+        let delLi=e1.target.parentElement;
+        userlist.removeChild(delLi);
+        localStorage.removeItem(delLi.id);
+    }  
 }
