c02b4838e90627368c8025589e3a7da0801363aa
aknair95 <aakashunair01031995@gmail.com>
2023-01-30 07:41:25 +0530

    task-12

--- formUserInfo.js
+++ formUserInfo.js
@@ -1,15 +1,27 @@
 let userName=document.getElementById('nameInput');
 let userEmail=document.getElementById('emailInput');
+let userMobileNo=document.getElementById('mobileNo');
 let userform=document.getElementById('addform');
+let userlist=document.getElementById('userInfoList');
 
-userform.addEventListener('submit',store);
+userform.addEventListener('submit',storeList);
 
-function store(e)
-{
+function storeList(e)
+{   
+    e.preventDefault();
     let userData={
         Name: userName.value,
-        Email: userEmail.value
+        Email: userEmail.value,
+        MobileNo : userMobileNo.value
     }
     let UserDataString=JSON.stringify(userData);
-    localStorage.setItem('UserData',UserDataString);
+    localStorage.setItem(userMobileNo.value,UserDataString);
+    let liTag=document.createElement('li');
+    liTag.className='list-group-item';
+    let liText=document.createTextNode(`Name-${userName.value} EmailID-${userEmail.value} Mobile No-${userMobileNo.value}`);
+    liTag.appendChild(liText);
+    userlist.appendChild(liTag);
+    userName.value='';
+    userEmail.value='';
+    userMobileNo.value='';
 }
