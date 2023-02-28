36b8c9c22cb82fce649738402b756427d7c5319e
aknair95 <aakashunair01031995@gmail.com>
2023-02-28 15:22:58 +0530

    final

--- js-expesnse-tracker.js
+++ js-expesnse-tracker.js
@@ -1,18 +1,30 @@
 let form=document.querySelector('#addexpense');
 let expList=document.querySelector('#expenselist');
+let exp=document.querySelector('#expense');
+let desc=document.getElementById('description');
+let cat=document.querySelector('#category');
+let sno=document.querySelector('#sNo');
 
 form.addEventListener('submit',addItem);
-expList.addEventListener('click',removeItem);
+expList.addEventListener('click',deleteItem);
 expList.addEventListener('click',editItem);
+
 function addItem(e)
 {   
     e.preventDefault();
-    let exp=document.querySelector('#expense');
-    let desc=document.getElementById('description');
-    let cat=document.querySelector('#category');
+   
+    let expData={
+        Sno: sno.value, 
+        Expense: exp.value,
+        Description: desc.value,
+        Category: cat.value
+    }
+    let expDataString=JSON.stringify(expData);
+    localStorage.setItem(sno.value,expDataString);
 
     let liTag=document.createElement('li');
     liTag.className="list-group-item";
+    liTag.id=sno.value;
 
     let del=document.createElement('button');
     del.className="btn btn-danger btn-sm float-right delete";
@@ -25,46 +37,62 @@ function addItem(e)
     edit.style='margin-right: 10px;background-color: lightgreen;margin-left: 1%;';
     editText=document.createTextNode('Edit Expense');
     edit.appendChild(editText);
-    
+
+    let liSno=document.createTextNode(sno.value);
     let liTextExp=document.createTextNode(exp.value);
     let liTextDesc=document.createTextNode(desc.value);
     let liTextCat=document.createTextNode(cat.value);
-    let gap=document.createTextNode(' ');
+    let gap=document.createTextNode('-');
     let gap1=document.createTextNode(' ');
+    let gap2=document.createTextNode(' ');
     
-    liTag.appendChild(liTextExp);
+    liTag.appendChild(liSno);
     liTag.appendChild(gap);
-    liTag.appendChild(liTextDesc);
+    liTag.appendChild(liTextExp);
     liTag.appendChild(gap1);
+    liTag.appendChild(liTextDesc);
+    liTag.appendChild(gap2);
     liTag.appendChild(liTextCat);
     liTag.appendChild(edit);
     liTag.appendChild(del);
 
     expList.appendChild(liTag);
+    sno.value=0;
     exp.value='';
     desc.value='';
     cat.value='';
 }
 
-function removeItem(e1)
+function deleteItem(e1)
 {
     if(e1.target.classList.contains('btn-danger'))
     {
-        let li=e1.target.parentElement;
-        expList.removeChild(li);
+        let lidel=e1.target.parentElement;
+        expList.removeChild(lidel);
+        localStorage.removeItem(lidel.id);
     }
+    
 }
 
 function editItem(e2)
-{
-    let exp1=document.querySelector('#expense');
-    let desc1=document.getElementById('description');
-    let cat1=document.querySelector('#category');
-    exp1.value=exp1.value;
-    desc1.value='';
-    cat1.value='';
+{   
+    if(e2.target.classList.contains('edit'))
+    {
+        let liedit=e2.target.parentElement;
+        expList.removeChild(liedit);
+
+        let LSvalue=localStorage.getItem(liedit.id);
+        let LSvalueObj=JSON.parse(LSvalue);
+
+        localStorage.removeItem(liedit.id);
+        sno.value=liedit.id;
+        exp.value=LSvalueObj.Expense;
+        desc.value=LSvalueObj.Description;
+        cat.value=LSvalueObj.Category;
+          
+    }
 }
 
-}
+
 
 
