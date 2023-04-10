783281242d1ce36ad0f2bd5d0ecafd5e105fbac5
aknair95 <aakashunair01031995@gmail.com>
2023-04-11 04:59:59 +0530

    aysnc/await  axios request instead of promise/then

--- js-restAPI-test-TODO/todoList.js
+++ js-restAPI-test-TODO/todoList.js
@@ -1,3 +1,4 @@
+
 let todoName=document.getElementById("todo");
 let description=document.getElementById("descript");
 let addItem=document.getElementById("todoForm");
@@ -7,24 +8,31 @@ let todoList2=document.getElementById("todoList2");
 
 addItem.addEventListener('submit',addTodo);
 window.addEventListener("DOMContentLoaded",reload);
-todoList1.addEventListener("click",removeTodo);
+todoList1.addEventListener("click",removeTodoRemain);
+todoList2.addEventListener("click",removeTodoDone);
 todoList1.addEventListener("click",doneTodo);
 
-function addTodo(e)
+
+async function addTodo(e)
 {   
     e.preventDefault();
     let todoObj={
         TodoName: todoName.value,
-        Description: description.value
+        Description: description.value,
+        status: "pending"
     };
 
-    axios.post("https://crudcrud.com/api/40593fa1b4e04828898140e0f2d30520/todos",todoObj)
-    .then((res)=> console.log(res))
-    .catch((err)=>console.log(err));
+    try{
+        const res=await axios.post("https://crudcrud.com/api/c3e062260ebd46d0a3eaf1595794f120/todos",todoObj);
+        console.log(res);
+    }catch(err){        
+        console.log(err);
+    }
 
     let liTag1=document.createElement("li");
     liTag1.id=todoObj._id;
     liTag1.className="list-group-item";
+    liTag1.setAttribute("status","pending");
     let liText1=document.createTextNode(`Todo Name-${todoName.value} Description-${description.value}`);
 
     doneBtn=document.createElement("input");
@@ -49,25 +57,27 @@ function addTodo(e)
 
 }
 
-function reload(e1)
+async function reload(e1)
 {
-    axios.get("https://crudcrud.com/api/40593fa1b4e04828898140e0f2d30520/todos")
-    .then((res)=> {
+    try{
+        const res=await axios.get("https://crudcrud.com/api/c3e062260ebd46d0a3eaf1595794f120/todos");
         for(let i=0;i<res.data.length;i++)
         {
             showTodoList(res.data[i]);
         }
-    })
-    .catch((err)=> console.log(err));
+    }
+    catch(err){
+        console.log(err);
+    }
      
 }
 
 function showTodoList(todoData)
 {
-    let liTag1=document.createElement("li");
-    liTag1.id=todoData._id;
-    liTag1.className="list-group-item";
-    let liText1=document.createTextNode(`Todo Name-${todoData.TodoName} Description-${todoData.Description}`);
+    let liTag=document.createElement("li");
+    liTag.id=todoData._id;
+    liTag.className="list-group-item";
+    let liText=document.createTextNode(`Todo Name-${todoData.TodoName} Description-${todoData.Description}`);
 
     doneBtn=document.createElement("input");
     doneBtn.type='button';
@@ -81,32 +91,90 @@ function showTodoList(todoData)
     delBtn.className="delete-btn";
     delBtn.value="DELETE";
 
-    liTag1.appendChild(liText1);
-    liTag1.appendChild(doneBtn);
-    liTag1.appendChild(delBtn);
-    todoList1.appendChild(liTag1);
+    liTag.appendChild(liText);
+    liTag.appendChild(doneBtn);
+    liTag.appendChild(delBtn);
+
+    if(todoData.status==="pending")
+    {
+        todoList1.appendChild(liTag);
+    }
+    else{
+        liTag.removeChild(doneBtn);
+        todoList2.appendChild(liTag);
+    }
+    
 }
-function removeTodo(e2)
+async function removeTodoRemain(e2)
 {   
     if(e2.target.classList.contains('delete-btn'))
     {
         let delLi=e2.target.parentElement;
-        axios.delete(`https://crudcrud.com/api/40593fa1b4e04828898140e0f2d30520/todos/${delLi.id}`)
-        .then((res)=> console.log(res))
-        .catch((err)=> console.log(err));
-          
+        try{
+            const res=await axios.delete(`https://crudcrud.com/api/c3e062260ebd46d0a3eaf1595794f120/todos/${delLi.id}`);
+            console.log(res);
+        }
+        catch(err){
+            console.log(err);
+        } 
         todoList1.removeChild(delLi);    
     }  
 }
 
-function doneTodo(e3)
+async function removeTodoDone(e3)
+{   
+    if(e3.target.classList.contains('delete-btn'))
+    {
+        let delLi=e3.target.parentElement;
+        try{
+            const res=await axios.delete(`https://crudcrud.com/api/c3e062260ebd46d0a3eaf1595794f120/todos/${delLi.id}`);
+            console.log(res);
+        }
+        catch(err){
+            console.log(err);
+        }   
+        todoList2.removeChild(delLi);    
+    }  
+}
+
+async function doneTodo(e4)
 {
-    if(e3.target.classList.contains('done-btn'))
+    if(e4.target.classList.contains('done-btn'))
     {   
-        let doneLi=e3.target.parentElement;
-        doneLi.removeChild(e3.target);
-        todoList2.appendChild(doneLi);
+        let todoObj1={
+            TodoName: "",
+            Description: "",
+            status: "done"
+        } 
+
+        let doneLi=e4.target.parentElement;
+        doneLi.status="done";
+
+        try{
+            const res1=await axios.get(`https://crudcrud.com/api/c3e062260ebd46d0a3eaf1595794f120/todos/${doneLi.id}`);
+            console.log(res1.data);
+            todoObj1={
+                TodoName: res1.data.TodoName,
+                Description: res1.data.Description,
+            }
+        }
+        catch(err){
+            console.log(err);
+        }
+
+        try{
+            const res2=await axios.put(`https://crudcrud.com/api/c3e062260ebd46d0a3eaf1595794f120/todos/${doneLi.id}`,todoObj1);
+            console.log(res2);
+        }
+        catch(err)
+        {
+            console.log(err);
+        }
+
+        doneLi.removeChild(e4.target);
         todoList1.removeChild(doneLi); 
+        todoList2.appendChild(doneLi);
+       
     }
    
 }
